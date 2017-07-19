4818cfbe346c72b466bf17d1d81434656246acfe
philcast <philippe.castelli@gmail.com>
2017-07-19 11:22:03 +0200

    remove unecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import RepositoryList from 'components/RepositoryList';
-import './App.css';
 
-// const logo = require('./logo.svg');
+import './App.css';
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <div className="app">
         <div className="app-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h2>Welcome to Github Explorer</h2>
         </div>
         <RepositoryList />
